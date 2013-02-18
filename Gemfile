01a86f83dd7e73ede5f0ef860df2a53b197af799
Ubuntu <ubuntu@ip-10-252-109-170.us-west-2.compute.internal>
2013-02-18 02:15:10 +0000

    add xmpp4r

--- Gemfile
+++ Gemfile
@@ -9,6 +9,12 @@ gem 'mysql2'
 
 gem 'devise'
 
+gem 'xmpp4r'
+
+gem 'therubyracer'
+gem 'linecache19', :git => 'git://github.com/mark-moseley/linecache'
+gem 'ruby-debug-base19x', '~> 0.11.30.pre4'
+gem 'ruby-debug19'
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,26 @@
+require 'xmpp4r'
+
 class RegistrationsController < Devise::RegistrationsController
+  def register_XMPP_user(username, password)
+    jid = Jabber::JID.new('chat@localhost')
+    client = Jabber::Client.new(jid)
+    client.connect
+    client.auth("123456")
+
+    #iqr = Iq.new(:set)
+    #qr = IqQuery.new
+    #qr.add_namespace('jabber:iq:register')
+    #qr.add(REXML::Element.new('username').add_text(username))
+    #qr.add(REXML::Element.new('password').add_text(password))
+    #iqr.add(qr)
+    #client.send iqr
+  end
+
   def create
       params[:user][:email] = params[:user][:email].strip if params[:user] and params[:user][:email]
       build_resource
       logger.info '=================resource==========' + params[:user][:password]
+      register_XMPP_user params[:user][:email], params[:user][:password]
       if resource.save
         if resource.active_for_authentication?
           set_flash_message :notice, :signed_up if is_navigational_format?
