67fd399a6caaca48a2f69c43640ce461a6334c47
ccwei <chihchiang.wei@gmail.com>
2013-04-20 08:17:55 +0000

    fix image on FF, add xmpp4r

--- Gemfile
+++ Gemfile
@@ -14,6 +14,8 @@ gem "therubyracer"
 gem "less-rails" #Sprockets (what Rails 3.1 uses for its asset pipeline) supports LESS
 gem "twitter-bootstrap-rails"
 
+gem 'xmpp4r'
+
 gem 'devise'
 
 gem 'therubyracer'
--- app/controllers/chatusers_controller.rb
+++ app/controllers/chatusers_controller.rb
@@ -19,6 +19,22 @@ class ChatusersController < ActionController::Base
     user1 = params[:user]
     user2 = random_user
     add_rosteritem user1, user2
+    send_default_msg 'a', 'b'
     render :json => {:listenerJid => user2.jid + '@' + DOMAINNAME, :nickname => user2.nickname, :desc => user2.description, :gender => user2.gender}.to_json()
+  end
+
+  def send_default_msg from, to
+	Jabber::debug = true # Uncomment this if you want to see what's being sent and received!
+	jid = Jabber::JID::new('ccwei_stanford.edu@textsupport.no-ip.org')
+	password = '123456'
+	cl = Jabber::Client::new(jid)
+	cl.connect
+	cl.auth(password)
+
+	to = "kkk_gmail.com@textsupport.no-ip.org"
+	subject = "XMPP4R test"
+	body = "Hi, this is my first try from XMPP4R!!!"
+	m = Jabber::Message::new(to, body).set_type(:normal).set_id('1').set_subject(subject)
+	cl.send m
   end
 end
